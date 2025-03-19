96dce83ce515e02eac0325c7277d9b95d38f1826
David Xiang <admin@mdavid.cn>
2025-03-19 18:53:09 +0800

    bugFix: priceScaleId

--- lightweight_charts/abstract.py
+++ lightweight_charts/abstract.py
@@ -508,7 +508,7 @@ class Line(SeriesCommon):
                     lastValueVisible: {jbool(price_label)},
                     priceLineVisible: {jbool(price_line)},
                     crosshairMarkerVisible: {jbool(crosshair_marker)},
-                    priceScaleId: {f'{price_scale_id}' if price_scale_id else 'undefined'},
+                    priceScaleId: {f'"{price_scale_id}"' if price_scale_id else 'undefined'},
                     {"""autoscaleInfoProvider: () => ({
                             priceRange: {
                                 minValue: 1_000_000_000,
@@ -582,7 +582,7 @@ class Histogram(SeriesCommon):
                 color: '{color}',
                 lastValueVisible: {jbool(price_label)},
                 priceLineVisible: {jbool(price_line)},
-                priceScaleId: {f'{price_scale_id}' if price_scale_id else f'{self.id}'},
+                priceScaleId: {f'"{price_scale_id}"' if price_scale_id else f'{self.id}'},
                 priceFormat: {{type: "volume"}},
             }},
             {f'{pane_index}' if pane_index is not None else '0'}
